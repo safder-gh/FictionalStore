78831b26d8b106bb00975e7fe4a9870e428fc738
safderghauri <safder.ghauri@eurosofttech.co.uk>
2025-08-15 13:15:28 +0500

    Small Change

--- product-service/src/main/java/com/fs/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/fs/product_service/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.fs.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.fs.product_service.interfaces")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
